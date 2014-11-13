7d6be8b10cd9c1ed2eeb607359df14971407d399
Tomasz <tomasz.cichoszewski.x@gmail.com>
2014-11-13 01:09:05 +0100

    Uzupełniona klasa directions, manifest usunięty z wersjonowania

--- PoliGdzie/src/com/json/poligdzie/Directions.java
+++ PoliGdzie/src/com/json/poligdzie/Directions.java
@@ -1,10 +1,25 @@
 package com.json.poligdzie;
 
+import java.util.ArrayList;
+
 
 
 public class Directions {
 
 	private String status;
-	private Routes routes;
+	private ArrayList <Routes> routes;
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public ArrayList<Routes> getRoutes() {
+		return routes;
+	}
+	public void setRoutes(ArrayList<Routes> routes) {
+		this.routes = routes;
+	}
 	
 }
