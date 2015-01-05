c72b84581c56678541f630f88768e16b61505b09
Tomasz <tomasz.cichoszewski.x@gmail.com>
2015-01-05 22:11:23 +0100

    zmieniony numer bazy

--- PoliGdzie/src/com/poligdzie/interfaces/Constants.java
+++ PoliGdzie/src/com/poligdzie/interfaces/Constants.java
@@ -5,7 +5,7 @@ public interface Constants
 
 	// baza danych
 	public String	DATABASE_NAME			= "Poligdzie.db";
-	public int		DATABASE_VERSION		= 17;
+	public int		DATABASE_VERSION		= 18;
 
 	// Ustawienia aplikacji w PreferenceManager
 	public String	START_FIELD_VALUE		= "";
