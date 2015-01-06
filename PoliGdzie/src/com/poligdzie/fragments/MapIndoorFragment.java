215daef54747db5fdaf004114f38485e7a63c8e9
Wojtek <7viller@wp.pl>
2015-01-06 18:06:12 +0100

    Asynchroniczne ładowanie mapy

--- PoliGdzie/src/com/poligdzie/fragments/MapIndoorFragment.java
+++ PoliGdzie/src/com/poligdzie/fragments/MapIndoorFragment.java
@@ -8,7 +8,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
+import com.example.poligdzie.R;
 import com.poligdzie.base.PoliGdzieMapFragment;
+import com.poligdzie.tasks.BitmapWorkerTask;
 import com.poligdzie.widgets.BuildingImageView;
 
 public class MapIndoorFragment extends PoliGdzieMapFragment implements
@@ -23,27 +25,29 @@ public class MapIndoorFragment extends PoliGdzieMapFragment implements
 			Bundle savedInstanceState)
 	{
 		super.onCreateView(inflater, container, savedInstanceState);
-		buildingImage = new BuildingImageView(getActivity());
-
-		BitmapFactory.Options options = new BitmapFactory.Options();
-		options.inSampleSize = 2;
-		options.inDither = false; // Disable Dithering mode
-		options.inPurgeable = true; // Tell to gc that whether it needs free
-									// memory, the Bitmap can be cleared
-
-		Bitmap bmp = BitmapFactory.decodeResource(getResources(),
-				this.getDrawableId(this.drawableId, this.getActivity()),
-				options);
-
-		buildingImage.setBitmap(bmp);
-
-		return buildingImage;
+		
+		View rootView = inflater.inflate(R.layout.map_indoor_fragment,
+				container, false);
+		
+		BitmapWorkerTask bitmapWorker = new BitmapWorkerTask(this,getActivity(),getActivity().getResources());
+		bitmapWorker.execute(getDrawableId(drawableId, getActivity()));
+		
+		return rootView;
 	}
 
 	@Override
 	public void onClick(View v)
 	{
 
+	}
+	
+	public void setFloorImage(Bitmap bitmap)
+	{
+		ViewGroup container = (ViewGroup) getView().findViewById(R.id.indoor_map_container);
+		   container.removeAllViews();
+		   View inflater = LayoutInflater.from(getActivity()).inflate(R.layout.map_indoor_floor_scheme, container);
+		   buildingImage = (BuildingImageView) inflater.findViewById(R.id.imageview_floor_scheme);
+		   buildingImage.setBitmap(bitmap);
 	}
 
 	public MapIndoorFragment()
---
+++ PoliGdzie/src/com/poligdzie/tasks/BitmapWorkerTask.java
@@ -0,0 +1,55 @@
+package com.poligdzie.tasks;
+
+import java.lang.ref.WeakReference;
+
+import com.poligdzie.fragments.MapIndoorFragment;
+import com.poligdzie.widgets.BuildingImageView;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.renderscript.RenderScript.Priority;
+import android.util.Log;
+
+public class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
+    private final WeakReference<BuildingImageView> buildingViewReference;
+    private int data = 0;
+    private Resources resources;
+    private MapIndoorFragment fragment;
+    
+
+    public BitmapWorkerTask(MapIndoorFragment frag, Context context, Resources res) {
+        // Use a WeakReference to ensure the ImageView can be garbage collected
+    	BuildingImageView building = new BuildingImageView(context);
+    	buildingViewReference = new WeakReference<BuildingImageView>(building);
+        resources = res;
+        fragment = frag;
+    }
+
+    // Decode image in background.
+    @Override
+    protected Bitmap doInBackground(Integer... params) {
+    	Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+        data = params[0];
+        BitmapFactory.Options options=new BitmapFactory.Options();
+		options.inSampleSize = 2;
+		options.inDither=false;                     
+		options.inPurgeable=true;  
+		Log.i("t","1");
+		Bitmap bitmap = BitmapFactory.decodeResource(resources,data, options);
+		Log.i("t","2");        
+		return bitmap;
+    }
+
+    // Once complete, see if ImageView is still around and set bitmap.
+    @Override
+    protected void onPostExecute(Bitmap bitmap) 
+    {
+    	if(bitmap != null)
+    	{
+    		fragment.setFloorImage(bitmap);
+    	}
+    }
+}
