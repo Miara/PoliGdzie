4c6e6e494a35d3d8861042c011d32c7c01f44e96
Tomasz <tomasz.cichoszewski.x@gmail.com>
2015-01-20 21:05:32 +0100

    dodane chowanie info window po ruchu

--- PoliGdzie/src/com/poligdzie/fragments/MapIndoorFragment.java
+++ PoliGdzie/src/com/poligdzie/fragments/MapIndoorFragment.java
@@ -27,21 +27,21 @@ public class MapIndoorFragment extends PoliGdzieMapFragment implements
 
 	private BuildingImageView	buildingImage;
 	private int					floorId;
-	private List<Line> routeLines = new ArrayList<Line>();
+	private List<Line>			routeLines	= new ArrayList<Line>();
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState)
 	{
 		super.onCreateView(inflater, container, savedInstanceState);
-		
+
 		View rootView = inflater.inflate(R.layout.map_indoor_fragment,
 				container, false);
-		
-		BitmapWorkerTask bitmapWorker = new BitmapWorkerTask(this,getActivity(),
-				getActivity().getResources());
+
+		BitmapWorkerTask bitmapWorker = new BitmapWorkerTask(this,
+				getActivity(), getActivity().getResources());
 		bitmapWorker.execute(getDrawableId(drawableId, getActivity()));
-		
+
 		return rootView;
 	}
 
@@ -50,27 +50,30 @@ public class MapIndoorFragment extends PoliGdzieMapFragment implements
 	{
 
 	}
-	
+
 	public void setFloorImage(Bitmap bitmap)
 	{
-		ViewGroup container = (ViewGroup) getView().findViewById(R.id.indoor_map_container);
-		   container.removeAllViews();
-		   View inflater = LayoutInflater.from(getActivity()).inflate(R.layout.map_indoor_floor_scheme, container);
-		  
-		   
-		   buildingImage = (BuildingImageView) inflater.findViewById(R.id.imageview_floor_scheme);
-		   buildingImage.setBitmap(bitmap);
-		   try
-			{
-				Floor floor = dbHelper.getFloorDao().queryForId(floorId);
+		ViewGroup container = (ViewGroup) getView().findViewById(
+				R.id.indoor_map_container);
+		container.removeAllViews();
+		View inflater = LayoutInflater.from(getActivity()).inflate(
+				R.layout.map_indoor_floor_scheme, container);
+
+		buildingImage = (BuildingImageView) inflater
+				.findViewById(R.id.imageview_floor_scheme);
+		buildingImage.setBitmap(bitmap);
+		try
+		{
+			Floor floor = dbHelper.getFloorDao().queryForId(floorId);
+			if (routeLines.size() > 0)
 				buildingImage.setLines(routeLines);
-				buildingImage.setOriginalWidth(floor.getWidth());
-				buildingImage.setOriginalHeight(floor.getHeight());
-			} catch (SQLException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			buildingImage.setOriginalWidth(floor.getWidth());
+			buildingImage.setOriginalHeight(floor.getHeight());
+		} catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public MapIndoorFragment()
@@ -100,27 +103,27 @@ public class MapIndoorFragment extends PoliGdzieMapFragment implements
 	{
 		super(drawableId, name, viewTag);
 		this.floorId = floorId;
-		Log.i("poligdzie","floor:"+floorId);
-		
-		if(points.size() > 1)
+		Log.i("poligdzie", "floor:" + floorId);
+
+		if (points.size() > 1)
 		{
-			boolean first= true;
+			boolean first = true;
 			NavigationPoint previous = null;
-			for(NavigationPoint current: points)
+			for (NavigationPoint current : points)
 			{
-				Log.i("poligdzie","current:"+current.getId());
-				if(first)
+				Log.i("poligdzie", "current:" + current.getId());
+				if (first)
 				{
 					previous = current;
 					first = false;
-				}
-				else
+				} else
 				{
-					routeLines.add(new Line(previous.getCoordX(),previous.getCoordY(),
-							current.getCoordX(),current.getCoordY()));
+					routeLines.add(new Line(previous.getCoordX(), previous
+							.getCoordY(), current.getCoordX(), current
+							.getCoordY()));
 					previous = current;
 				}
-				
+
 			}
 		}
 	}
